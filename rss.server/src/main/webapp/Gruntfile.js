4b64fa0e2b82dfb0402f7a52814693bbb5c11f99
Bill Blake <billblake01@yahoo.ie>
2014-07-07 23:31:31 +0100

    Added grunt file

---
+++ rss.server/src/main/webapp/Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      app: {
+        src: ["app/app.js",
+              "app/controllers/controllers.js",
+              "app/controllers/listController.js",
+              "app/controllers/signUpController.js",
+              "app/services/feedService.js",
+              "app/services/userService.js",
+              "app/directives/animatedView.js",
+              "app/filters/filters.js"],
+        dest: 'scripts/main.js',
+      },
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'Content/main.css': 'Content/sass/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['app/**/*.js'],
+        tasks: ['concat']
+      },
+      styles: {
+        files: ['./**/*.scss'],
+        tasks: ['sass']
+      }
+    },
+
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'src/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify', 'concat', 'sass', 'watch']);
+  grunt.registerTask('default', ['watch']);
+
+};
