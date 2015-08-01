e84093aeb9ec8ccd8c0460f8a644882d78fbfb25
Bill Blake <billblake01@yahoo.ie>
2015-08-01 23:25:41 +0100

    First attempt at integrating jasmine

---
+++ rss.server/src/test/webapp/js/specs/feedManagerControllerSpec.js
@@ -0,0 +1,14 @@
+describe("Crisper", function() {
+//    beforeEach(module('app'));
+//
+//    var $controller;
+//
+//    beforeEach(inject(function(_$controller_){
+//      // The injector unwraps the underscores (_) from around the parameter names when matching
+//      $controller = _$controller_;
+//    }));
+
+    it("should be a constructor function", function() {
+        expect(true).toBe(true);
+    });
+});
